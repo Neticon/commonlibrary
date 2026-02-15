366a2181bdab2a2844789a6899f9ff0ebd1067f1
Djordje Stojakovic <d.stojakovic@neticon.it>
2026-02-15 23:26:37 +0100

    1.0.48-logModel

--- Models/LogModel.cs
+++ Models/LogModel.cs
@@ -12,7 +12,7 @@
         public string operation_type { get; set; }
         public string entity { get; set; }
         public string message { get; set; }
-        public string client_addr { get; set; }
-        public int client_port { get; set; }
+        public string? client_addr { get; set; }
+        public int? client_port { get; set; }
     }
 }
