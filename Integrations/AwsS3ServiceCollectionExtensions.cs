ccd2dc75a16adfe4ed394ce8625a3d0176710cce
Djordje Stojakovic <d.stojakovic@neticon.it>
2026-03-06 13:30:49 +0100

    1.0.96-s3Debug

--- Integrations/AwsS3ServiceCollectionExtensions.cs
+++ Integrations/AwsS3ServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
+using Amazon.SecurityToken.Model;
+using Amazon.SecurityToken;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 public static class AwsS3ServiceCollectionExtensions
 {
@@ -10,7 +13,7 @@ public static class AwsS3ServiceCollectionExtensions
         string roleArn,
         RegionEndpoint region)
     {
-        services.AddSingleton<IAmazonS3>(_ =>
+        services.AddSingleton<Task<IAmazonS3>>(async _ =>
         {
             Console.WriteLine("TOKEN" + Environment.GetEnvironmentVariable("AWS_WEB_IDENTITY_TOKEN_FILE"));
             Console.WriteLine("ARN" + Environment.GetEnvironmentVariable("AWS_ROLE_ARN"));
@@ -20,12 +23,23 @@ public static class AwsS3ServiceCollectionExtensions
                 Environment.GetEnvironmentVariable("AWS_ROLE_ARN"), $"IRSA-{Guid.NewGuid().ToString("N").Substring(0, 8)}"
             );
 
+            using var stsClient = new AmazonSecurityTokenServiceClient(sourceCredentials, RegionEndpoint.EUCentral1);
+
+            // Check if the token works
+            var identity = await stsClient.GetCallerIdentityAsync(new GetCallerIdentityRequest());
+            Console.WriteLine(identity.Arn);
+
             //var assumedCredentials = new AssumeRoleAWSCredentials(
             //    sourceCredentials,
             //    roleArn,
             //    $"S3Session-{Environment.MachineName}"
             //);
-
+            using var s3Client = new AmazonS3Client(sourceCredentials, RegionEndpoint.EUCentral1);
+            var buckets = await s3Client.ListBucketsAsync();
+            foreach (var b in buckets.Buckets)
+            {
+                Console.WriteLine(b.BucketName);
+            }
             return new AmazonS3Client(sourceCredentials, region);
         });
 
