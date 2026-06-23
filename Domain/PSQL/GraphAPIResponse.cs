fbcee4f2a7904292b0aeb59f1a1d493a5f9c632a
Djordje Stojakovic <d.stojakovic@neticon.it>
2026-06-23 14:19:48 +0200

    2.0.71-GraphApiResponseAddedVmFields

--- Domain/PSQL/GraphAPIResponse.cs
+++ Domain/PSQL/GraphAPIResponse.cs
@@ -17,10 +17,27 @@
         public int? per_page { get; set; }
         public int? total_pages { get; set; }
         public string? org_code { get; set; }
+        public List<string> statuses { get; set; }
+        public List<string> product_plans { get; set; }
     }
 
     public class BulkGraphAPIResponse<T>
     {
         public List<GraphAPIResponse<T>> results { get; set; }
+    }
+
+    public class TabulardataAPIResponse
+    {
+        public List<List<object>> rows { get; set; }
+        public List<string> headings { get; set; }
+        public bool success { get; set; }
+        public Guid request_id { get; set; }
+        public int? count { get; set; }
+        public string? entity { get; set; }
+        public string? schema { get; set; }
+        public string? period { get; set; }
+        public string? start_date { get; set; }
+        public string? end_date { get; set; }
+        public Guid? tenant_id { get; set; }
     }
 }
