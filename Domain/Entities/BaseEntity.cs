72ceeafc753c1a24dccd8137521b5bf433a460e5
Djordje Stojakovic <d.stojakovic@neticon.it>
2026-06-29 16:06:08 +0200

    2.0.78-BaseEntityRemoveUnusedEncryptionFields

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -6,18 +6,11 @@
         public virtual string _table { get; }
         [IgnoreForSerialization]
         public virtual string _schema { get; }
-        public virtual List<string> _encryptedFields { get; }
 
         public static (string _table, string _schema) GetMeta<T>() where T : BaseEntity, new()
         {
             var entity = new T();
             return (entity._table, entity._schema);
-        }
-
-        public static List<string> GetEncryptedFields<T>() where T : BaseEntity, new()
-        {
-            var entity = new T();
-            return (entity._encryptedFields);
         }
     }
 }
